85e715b0f347d5349958e55fd6d224428a29c794
Vincent Jared <vincentjared18@gmail.com>
2025-08-24 04:03:17 -0400

    completed the Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
+import Hero from "./sections/Hero"
 const App = () => {
   return (
-    <main>
-        <div>
-            <h1>Welcome to my website</h1>
-        </div>
-    </main>
+    <>
+      <Hero />
+    </>
     
   )
 }
